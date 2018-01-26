76e0562e78d362bbe4e2eb554b81df1c323fe97e
Matheus Ferraresi <matheusferraresi@live.com>
2018-01-26 11:34:53 +0000

    Minor fixes
    + Finished sticky menu on scroll
    + Fixed footer to sticky to the bottom
    + Responsive about-me section

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
 const navToggle = document.querySelector('.nav-toggle')
 const navMain = document.querySelector('.nav-main')
+const navMainUl = document.querySelector('.nav-main ul')
 const hamburger = document.querySelector('.hamburger')
+const header = document.querySelector('.header')
 
+// Menu hamburger toggle
 navToggle.addEventListener('click', () => {
     navMain.classList.toggle('is-open')
     hamburger.classList.toggle('is-open')
 });
+
+// Sticky menu on scroll
+window.addEventListener('scroll', () => {
+    let docScroll = document.documentElement.scrollTop;
+
+    if (docScroll > 50) {
+        header.classList.add('sticky');
+        navMainUl.classList.add('sticky-ul');
+    } else {
+        header.classList.remove('sticky');
+        navMainUl.classList.remove('sticky-ul');
+        
+    }
+})
