17ddc79fd221cee6632c5e11666c9bd965236f13
Matheus Ferraresi <matheusferraresi@live.com>
2018-01-26 13:19:43 +0000

    Fixed bugs
    + Fixed sticky scroll function to work just on desktops
    + Fixed minor bug on about-me section in small resolutions

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const navMain = document.querySelector('.nav-main')
 const navMainUl = document.querySelector('.nav-main ul')
 const hamburger = document.querySelector('.hamburger')
 const header = document.querySelector('.header')
+const mq = window.matchMedia( "(max-width: 990px)" )
 const menuLinks = document.querySelectorAll('nav [href]')
 
 // Menu hamburger toggle
@@ -12,18 +13,21 @@ navToggle.addEventListener('click', () => {
 });
 
 // Sticky menu on scroll
-window.addEventListener('scroll', () => {
-    let docScroll = document.documentElement.scrollTop;
+if (!mq) {
+    window.addEventListener('scroll', () => {
+        let docScroll = document.documentElement.scrollTop;
+    
+        if (docScroll > 50) {
+            header.classList.add('sticky');
+            navMainUl.classList.add('sticky-ul');
+        } else {
+            header.classList.remove('sticky');
+            navMainUl.classList.remove('sticky-ul');
+            
+        }
+    })
+}
 
-    if (docScroll > 50) {
-        header.classList.add('sticky');
-        navMainUl.classList.add('sticky-ul');
-    } else {
-        header.classList.remove('sticky');
-        navMainUl.classList.remove('sticky-ul');
-        
-    }
-})
 
 // Change active class on menu itens
 for (let i = 0; i < menuLinks.length; i++) {
